76c757d9d5ce92e95a863ae91838e9f1924250db
NONO <nonokub.671@gmail.com>
2022-12-09 16:04:56 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 using namespace std;
+#include<iostream>
+using namespace std;
+int n,nE = 0,nO = 0;
+int main(){
+    do{
+        cout << "Enter an integer: ";
+        cin >> n;
+        if(n%2 == 0 && n!= 0 ) nE++;
+        else if(n != 0 ) nO++;
+    }while(n!=0);
+    cout << "#Even numbers = " << nE << "\n";
+    cout << "#Odd numbers = " << nO;
+    return 0;
+}
 
 int main(){
     cout << "Enter an integer: ";
